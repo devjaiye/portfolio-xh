913378a9f0346f7b3c817c995cc74c11b511d456
dayojaiye74 <dayojaiye74@gmail.com>
2022-07-11 22:01:06 +0100

    portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+//..sticky scroll bar 
+const header = document.querySelector("header")
+
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky", window.scrollY > 0)
+})
+
