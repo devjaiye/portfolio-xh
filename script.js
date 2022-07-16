c66a9ef14f8fbad9021fb322318c8d741869259b
dayojaiye74 <dayojaiye74@gmail.com>
2022-07-16 23:05:31 +0100

    portfolio

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 0)
 })
 
-let menu = document.querySelector("#menu-icon")
-let navList = document.querySelector(".navlist")
+let menu = document.querySelector('#menu-icon')
+let navList = document.querySelector('.navlist')
 
-menu.onClick =() =>{
-    menu.classList.toggle("bx-x")
+menu.onclick =() =>{
+    menu.classList.toggle('bx-x')
     navList.classList.toggle('active')
+}
+
+windows.onscroll = () =>{
+    menu.classList.remove('bx-x')
+    navList.classList.remove('active')
 }
