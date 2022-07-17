9aa50f66fcffcf8f5d57a15eb6169f2aa9c80a8a
dayojaiye74 <dayojaiye74@gmail.com>
2022-07-17 23:20:39 +0100

    portfolio

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ window.onscroll = () =>{
     menu.classList.remove('bx-x')
     navList.classList.remove('active')
 }
+
+const sr = ScrollReveal({
+    distance: '45px',
+    duration: 2700,
+    reset: true
+})
+
+sr.reveal('.home-text',{
+    delay: 350,
+    origin: 'left'
+})
+
+sr.reveal('.home-img',{
+    delay: 350,
+    origin: 'right'
+})
+
+sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact',{
+    delay: 290,
+    origin: 'bottom'
+})
