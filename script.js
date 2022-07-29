180c87ce34348d6fcf1084f9323c31966a38e15b
dayojaiye74 <dayojaiye74@gmail.com>
2022-07-29 18:04:42 +0100

    portfolio

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 //..sticky scroll bar 
 const header = document.querySelector("header")
+twitter = document.querySelector("bx bxl-twitter")
+
+
+twitter.addEventListener("click", function(){
+    console.log("twitter clicked")
+})
 
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 0)
